8671f844cbaf30bb4e0bcea1988bb8a09d6bbd87
tlegoc <theo@legoc.net>
2023-07-07 14:34:17 +0200

    Deleted unecessary files

--- cmake-build-debug/_deps/glm-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/theo/Documents/CPP/vk_engine/cmake-build-debug/_deps/glm-src
-# Build directory: /home/theo/Documents/CPP/vk_engine/cmake-build-debug/_deps/glm-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
